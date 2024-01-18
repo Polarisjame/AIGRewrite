fef972a03dcd1c8e06b4c7dc29689f745eeaf7c7
Polarisjame <3464790978@qq.com>
2024-01-18 14:11:36 +0800

    update .gitignore

--- caseV/AOI.v
+++
@@ -1,6 +0,0 @@
-module AOI(A,B,C,D,F); 
-input A,B,C,D; 
-output F;
-wire A,B,C,D,F; 
-assign F= ~((A&B)|(C&D)); 
-endmodule
--- caseV/buried_ff.v
+++
@@ -1,9 +0,0 @@
-module buried_ff(c,b,a);
-output c;
-input b,a;
-reg c;
-always @(a or b)
-begin
-if((b==1)&&(a==1)) c=a&b;
-end
-endmodule
